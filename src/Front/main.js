c3c7da64a785130c71140a9212d8a189f9887d92
Cppshizoid <alex.krivoschecov@gmail.com>
2022-06-20 13:30:26 +0700

    Update main.js

--- src/Front/main.js
+++ src/Front/main.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function(){
+    $('.slide-list').slick({
+        autoplay: true,
+        autoplaySpeed: 2500,
+        dots: true,
+        appendDots: $('.nav-dots'),
+        infinite: true,
+    });
+});
